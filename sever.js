9d481d4562b153792de34d95756d7c580d48b663
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-11-16 14:09:37 -0600

    adds unique id generator path

--- sever.js
+++ sever.js
@@ -5,12 +5,13 @@ const PORT = 3030;
 
 app.use(express.static('public'));
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 
 require('./routes/api')(app);
-require('./routes/index')
+require('./routes/html')(app);
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
-})
+});
